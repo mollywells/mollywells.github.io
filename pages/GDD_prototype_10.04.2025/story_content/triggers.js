d4184d56ced2dcdb3cd67f540e4d347dd3b04990
mollywells <molly.wells@mail.utoronto.ca>
2025-04-10 11:43:08 -0400

    prototype push

---
+++ pages/GDD_prototype_10.04.2025/story_content/triggers.js
@@ -0,0 +1,39 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Jui89ByNdj":
+        Script1();
+        break;
+      case "6RIyydAVlVn":
+        Script2();
+        break;
+      case "5ymkmN92Y8k":
+        Script3();
+        break;
+      case "6lYNXRLDqGu":
+        Script4();
+        break;
+      case "5VhUhN2TgGz":
+        Script5();
+        break;
+      case "5XxHUizTRN6":
+        Script6();
+        break;
+      case "6kp7IsEtmXg":
+        Script7();
+        break;
+      case "6dM0SoRCAS6":
+        Script8();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
---
+++ pages/GDD_prototype_10.04.2025/story_content/user.js
@@ -0,0 +1,256 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script2 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script3 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script4 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script5 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script6 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script7 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script8 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+};
