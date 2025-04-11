8b55ae8df013a7fa31c3e07f7c7bf6a0c6df5e9d
mollywells <molly.wells@mail.utoronto.ca>
2025-04-11 16:11:59 -0400

    prototype

---
+++ pages/prototype/story_content/triggers.js
@@ -0,0 +1,60 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6EXA6cQcWKb":
+        Script1();
+        break;
+      case "69Zo5gd3pLK":
+        Script2();
+        break;
+      case "6DW14b8H85W":
+        Script3();
+        break;
+      case "6TADkIpFsnX":
+        Script4();
+        break;
+      case "5aS0z2bY3wY":
+        Script5();
+        break;
+      case "6qkclwP8llJ":
+        Script6();
+        break;
+      case "6nHDxj8CW2E":
+        Script7();
+        break;
+      case "6K73wNyxg7S":
+        Script8();
+        break;
+      case "5oxmXxKb2cn":
+        Script9();
+        break;
+      case "6Tqu0rD5VqU":
+        Script10();
+        break;
+      case "66I3kQpck5W":
+        Script11();
+        break;
+      case "5rE3A8RM3uP":
+        Script12();
+        break;
+      case "6Ozsxeb3tMS":
+        Script13();
+        break;
+      case "6ED0kwkseE8":
+        Script14();
+        break;
+      case "5pJuPIhMPUe":
+        Script15();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+};
