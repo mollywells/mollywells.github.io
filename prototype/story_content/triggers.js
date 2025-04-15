b648da099858dc1a6b5e60618681e85d1c33a084
mollywells <molly.wells@mail.utoronto.ca>
2025-04-15 12:07:40 -0400

    prototype

---
+++ prototype/story_content/triggers.js
@@ -0,0 +1,174 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hTHuy6I22d":
+        Script1();
+        break;
+      case "6JjeWmxO5EZ":
+        Script2();
+        break;
+      case "5sWlYCeA40R":
+        Script3();
+        break;
+      case "6o3V3yzJEXE":
+        Script4();
+        break;
+      case "6OV08pSp0qV":
+        Script5();
+        break;
+      case "5ttrfRjk6kW":
+        Script6();
+        break;
+      case "5yH6639TWH6":
+        Script7();
+        break;
+      case "6l4GVXzrL2U":
+        Script8();
+        break;
+      case "68Ah1RvWOun":
+        Script9();
+        break;
+      case "5zGSvqwKES4":
+        Script10();
+        break;
+      case "6oaBlGAzpi4":
+        Script11();
+        break;
+      case "6hHUV2Kk0X1":
+        Script12();
+        break;
+      case "632XRqLcVmL":
+        Script13();
+        break;
+      case "6aoGkSwGVE7":
+        Script14();
+        break;
+      case "5e9vIfjzOzf":
+        Script15();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6lumHi88PDz');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6lYlL8Qwoae';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('5dksrg2utpi');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5fafHetuZU6';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('5YFxDTL4uca');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6lYlL8Qwoae';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6IvatfChj4F');
+const duration = 1000;
+const easing = 'ease-out';
+const id = '6hiKQiGSsYP';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script5 = function()
+{
+  const target = object('65uDHYAA0yT');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6I53b1mPXNc';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script6 = function()
+{
+  const target = object('5VjE2ujpCN5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5bnLWkohDO1';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
---
+++ prototype/story_content/user.js
@@ -0,0 +1,287 @@
+window.InitUserScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script7 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=LandingPage', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script8 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=Introduction', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script9 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=MPAZones', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script10 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=DiveIn', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script11 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=Tutorial', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script12 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=Simulation&layername=inset', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script13 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=Simulation&layername=maps', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script14 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=Simulation&layername=cockpit', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+window.Script15 = function()
+{
+  console.log("hello world!")
+
+// Create a new XMLHttpRequest object
+var xhr = new XMLHttpRequest();
+
+// Configure it: GET-request for the URL /data
+xhr.open('GET', 'https://bmcresearch.utm.utoronto.ca/molly/index.php?slide_name=LandingPage', true);
+
+// Set up a function to handle the response
+xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        // Parse the JSON response
+        //var data = JSON.parse(xhr.responseText);
+        // Do something with the data
+        console.log(data);
+    } else {
+        console.error('Request failed with status:', xhr.status);
+    }
+};
+
+// Set up a function to handle errors
+xhr.onerror = function() {
+    console.error('Request failed');
+};
+
+// Send the request
+xhr.send();
+}
+
+};
